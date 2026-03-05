02d3b68e04744f1e133604dd6c60c9de6e30613e
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-03-05 19:38:09 +0200

    added return components and removed key element, that was not need

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,10 +3,11 @@ import './MovieList.scss';
 import { MovieCard } from '../MovieCard/MovieCard';
 
 export const MovieList = ({ movies }) => {
-  <div className="movies">
-    <h1>MovieList</h1>
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>;
+  return (
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
 };
